76e8f90f5bfcf482c35f99bbdd950094711b7032
atsb <44937323+atsb@users.noreply.github.com>
2022-11-14 15:22:52 +0100

    Stubbing fluidsynth
    This allows a clean re-implementation of another sound API.

--- src/engine/i_audio.h
+++ src/engine/i_audio.h
@@ -23,28 +23,5 @@
 #ifndef __I_AUDIO_H__
 #define __I_AUDIO_H__
 
-typedef struct {
-	fixed_t x;
-	fixed_t y;
-	fixed_t z;
-} sndsrc_t;
-
-int I_GetMaxChannels(void);
-int I_GetVoiceCount(void);
-sndsrc_t* I_GetSoundSource(int c);
-
-void I_InitSequencer(void);
-void I_ShutdownSound(void);
-void I_UpdateChannel(int c, int volume, int pan);
-void I_RemoveSoundSource(int c);
-void I_SetMusicVolume(float volume);
-void I_SetSoundVolume(float volume);
-void I_ResetSound(void);
-void I_PauseSound(void);
-void I_ResumeSound(void);
-void I_SetGain(float db);
-void I_StopSound(sndsrc_t* origin, int sfx_id);
-void I_StartMusic(int mus_id);
-void I_StartSound(int sfx_id, sndsrc_t* origin, int volume, int pan, int reverb);
 
 #endif // __I_AUDIO_H__
--- src/engine/s_sound.c
+++ src/engine/s_sound.c
@@ -45,51 +45,6 @@
 #include "i_audio.h"
 #include "con_console.h"
 
-// Adjustable by menu.
-#define NORM_VOLUME     127
-#define NORM_SEP        128
-
-// when to clip out sounds
-// Does not fit the large outdoor areas.
-#define S_CLIPPING_DIST (1700<<FRACBITS)
-
-// Distance to origin when sounds should be maxed out.
-// This should relate to movement clipping resolution
-// (see BLOCKMAP handling).
-
-#define S_MAX_DIST (NORM_VOLUME * (S_CLIPPING_DIST >> FRACBITS))    // [d64]
-#define S_CLOSE_DIST (200 << FRACBITS)
-#define S_ATTENUATOR ((S_CLIPPING_DIST - S_CLOSE_DIST) >> FRACBITS)
-
-#define S_PITCH_PERTURB         1
-#define S_STEREO_SWING          (96*0x10000)
-
-// percent attenuation from front to back
-#define S_IFRACVOL              30
-
-static dboolean nosound = false;
-static dboolean nomusic = false;
-static int lastmusic = 0;
-
-CVAR_CMD(s_sfxvol, 80) {
-	if (cvar->value < 0.0f) {
-		return;
-	}
-	S_SetSoundVolume(cvar->value);
-}
-CVAR_CMD(s_musvol, 80) {
-	if (cvar->value < 0.0f) {
-		return;
-	}
-	S_SetMusicVolume(cvar->value);
-}
-CVAR_CMD(s_gain, 1) {
-	if (cvar->value < 0.0f) {
-		return;
-	}
-	S_SetGainOutput(cvar->value);
-}
-
 //
 // Internals.
 //
@@ -103,25 +58,7 @@ int S_AdjustSoundParams(fixed_t x, fixed_t y, int* vol, int* sep);
 //
 
 void S_Init(void) {
-	if (M_CheckParm("-nosound")) {
-		nosound = true;
-		CON_DPrintf("Sounds disabled\n");
-	}
 
-	if (M_CheckParm("-nomusic")) {
-		nomusic = true;
-		CON_DPrintf("Music disabled\n");
-	}
-
-	if (nosound && nomusic) {
-		return;
-	}
-
-	I_InitSequencer();
-
-	S_SetMusicVolume(s_musvol.value);
-	S_SetSoundVolume(s_sfxvol.value);
-	S_SetGainOutput(s_gain.value);
 }
 
 //
@@ -129,7 +66,7 @@ void S_Init(void) {
 //
 
 void S_SetSoundVolume(float volume) {
-	I_SetSoundVolume(volume);
+
 }
 
 //
@@ -137,7 +74,7 @@ void S_SetSoundVolume(float volume) {
 //
 
 void S_SetMusicVolume(float volume) {
-	I_SetMusicVolume(volume);
+
 }
 
 //
@@ -145,7 +82,7 @@ void S_SetMusicVolume(float volume) {
 //
 
 void S_SetGainOutput(float db) {
-	I_SetGain(db);
+
 }
 
 //
@@ -153,16 +90,7 @@ void S_SetGainOutput(float db) {
 //
 
 void S_StartMusic(int mnum) {
-	if (nomusic) {
-		return;
-	}
 
-	if (mnum <= -1) {
-		return;
-	}
-
-	I_StartMusic(mnum);
-	lastmusic = mnum;
 }
 
 //
@@ -170,8 +98,7 @@ void S_StartMusic(int mnum) {
 //
 
 void S_StopMusic(void) {
-	I_StopSound(NULL, lastmusic);
-	lastmusic = 0;
+
 }
 
 //
@@ -179,19 +106,7 @@ void S_StopMusic(void) {
 //
 
 void S_ResetSound(void) {
-	int i;
 
-	if (nosound && nomusic) {
-		return;
-	}
-
-	I_ResetSound();
-
-	// villsa 12282013 - make sure we clear all sound sources
-	// during level transition
-	for (i = 0; i < I_GetMaxChannels(); i++) {
-		I_RemoveSoundSource(i);
-	}
 }
 
 //
@@ -199,11 +114,7 @@ void S_ResetSound(void) {
 //
 
 void S_PauseSound(void) {
-	if (nosound && nomusic) {
-		return;
-	}
 
-	I_PauseSound();
 }
 
 //
@@ -211,11 +122,7 @@ void S_PauseSound(void) {
 //
 
 void S_ResumeSound(void) {
-	if (nosound && nomusic) {
-		return;
-	}
 
-	I_ResumeSound();
 }
 
 //
@@ -223,7 +130,7 @@ void S_ResumeSound(void) {
 //
 
 void S_StopSound(mobj_t* origin, int sfx_id) {
-	I_StopSound((sndsrc_t*)origin, sfx_id);
+
 }
 
 //
@@ -231,7 +138,7 @@ void S_StopSound(mobj_t* origin, int sfx_id) {
 //
 
 int S_GetActiveSounds(void) {
-	return I_GetVoiceCount();
+
 }
 
 //
@@ -239,18 +146,7 @@ int S_GetActiveSounds(void) {
 //
 
 void S_RemoveOrigin(mobj_t* origin) {
-	int     channels;
-	mobj_t* source;
-	int     i;
 
-	channels = I_GetMaxChannels();
-
-	for (i = 0; i < channels; i++) {
-		source = (mobj_t*)I_GetSoundSource(i);
-		if (origin == source) {
-			I_RemoveSoundSource(i);
-		}
-	}
 }
 
 //
@@ -258,41 +154,7 @@ void S_RemoveOrigin(mobj_t* origin) {
 //
 
 void S_UpdateSounds(void) {
-	int     i;
-	int     audible;
-	int     volume;
-	int     sep;
-	mobj_t* source;
-	int     channels;
 
-	channels = I_GetMaxChannels();
-
-	for (i = 0; i < channels; i++) {
-		source = (mobj_t*)I_GetSoundSource(i);
-
-		if (source == NULL) {
-			continue;
-		}
-
-		// initialize parameters
-		volume = NORM_VOLUME;
-		sep = NORM_SEP;
-
-		// check non-local sounds for distance clipping
-		// or modify their params
-		if (source == players[consoleplayer].mo &&
-			players[consoleplayer].cameratarget == players[consoleplayer].mo) {
-			audible = 1;
-			sep = NORM_SEP;
-		}
-		else {
-			audible = S_AdjustSoundParams(source->x, source->y, &volume, &sep);
-		}
-
-		if (audible) {
-			I_UpdateChannel(i, volume, sep);
-		}
-	}
 }
 
 //
@@ -300,41 +162,7 @@ void S_UpdateSounds(void) {
 //
 
 void S_StartSound(mobj_t* origin, int sfx_id) {
-	int volume;
-	int sep;
-	int reverb;
 
-	if (nosound) {
-		return;
-	}
-
-	if (origin && origin != players[consoleplayer].cameratarget) {
-		if (!S_AdjustSoundParams(origin->x, origin->y, &volume, &sep)) {
-			return;
-		}
-	}
-	else {
-		sep = NORM_SEP;
-		volume = NORM_VOLUME;
-	}
-
-	reverb = 0;
-
-	if (origin) {
-		subsector_t* subsector;
-
-		subsector = R_PointInSubsector(origin->x, origin->y);
-
-		if (subsector->sector->flags & MS_REVERB) {
-			reverb = 16;
-		}
-		else if (subsector->sector->flags & MS_REVERBHEAVY) {
-			reverb = 32;
-		}
-	}
-
-	// Assigns the handle to one of the channels in the mix/output buffer.
-	I_StartSound(sfx_id, (sndsrc_t*)origin, volume, sep, reverb);
 }
 
 //
@@ -347,52 +175,7 @@ void S_StartSound(mobj_t* origin, int sfx_id) {
 //
 
 int S_AdjustSoundParams(fixed_t x, fixed_t y, int* vol, int* sep) {
-	fixed_t     approx_dist;
-	angle_t     angle;
-	mobj_t* listener;
-	player_t* player;
 
-	player = &players[consoleplayer];
-
-	listener = player->cameratarget;
-
-	// calculate the distance to sound origin
-	//  and clip it if necessary
-
-	// From _GG1_ p.428. Appox. eucledian distance fast.
-	approx_dist = P_AproxDistance(listener->x - x, listener->y - y);
-
-	if (approx_dist > S_CLIPPING_DIST) {
-		return 0;
-	}
-
-	if (listener->x != x || listener->y != y) {
-		// angle of source to listener
-		angle = R_PointToAngle2(listener->x, listener->y, x, y);
-
-		if (angle <= listener->angle) {
-			angle += 0xffffffff;
-		}
-		angle -= listener->angle;
-
-		// stereo separation
-		*sep = (NORM_VOLUME + 1) - (FixedMul(S_STEREO_SWING, dsin(angle)) >> FRACBITS);
-	}
-	else {
-		*sep = NORM_SEP;
-	}
-
-	// volume calculation
-	if (approx_dist < S_CLOSE_DIST) {
-		*vol = NORM_VOLUME;
-	}
-	else {
-		// distance effect
-		approx_dist >>= FRACBITS;
-		*vol = (((-approx_dist << 7) + (approx_dist)) + S_MAX_DIST) / S_ATTENUATOR;
-	}
-
-	return (*vol > 0);
 }
 
 //
@@ -403,9 +186,5 @@ CVAR_EXTERNAL(s_soundfont);
 CVAR_EXTERNAL(s_driver);
 
 void S_RegisterCvars(void) {
-	CON_CvarRegister(&s_sfxvol);
-	CON_CvarRegister(&s_musvol);
-	CON_CvarRegister(&s_gain);
 	CON_CvarRegister(&s_soundfont);
-	CON_CvarRegister(&s_driver);
 }
