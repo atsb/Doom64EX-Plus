7fbe038f88a61da9738152998c8a0ea69b7aec83
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2022-12-23 10:59:22 -0300

    Fix length

--- src/engine/m_misc.c
+++ src/engine/m_misc.c
@@ -212,7 +212,7 @@ int M_ReadFile(int8_t const* name, byte** buffer) {
 	errno = 0;
 
 	if ((fp = fopen(name, "rb"))) {
-		int length;
+		size_t length;
 
 		I_BeginRead();
 
@@ -222,7 +222,7 @@ int M_ReadFile(int8_t const* name, byte** buffer) {
 
 		*buffer = Z_Malloc(length, PU_STATIC, 0);
 
-		if (fread(*buffer, 1, sizeof(length), sizeof(fp)) == length) {
+		if (fread(*buffer, 1, length, sizeof(fp)) == length) {
 			fclose(fp);
 			return length;
 		}
