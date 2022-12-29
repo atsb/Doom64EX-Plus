53569d8f9fd31278e3f45253eeb3210b2c1795a5
atsb <44937323+atsb@users.noreply.github.com>
2022-12-29 11:57:00 +0100

    Making IDCLEV pistol start and adding EXCLEV

--- src/engine/m_cheat.c
+++ src/engine/m_cheat.c
@@ -52,6 +52,7 @@ typedef struct {
 static void M_CheatFa(player_t* player, int8_t dat[4]);
 static void M_CheatBerserk(player_t* player, int8_t dat[4]);
 static void M_CheatWarp(player_t* player, int8_t dat[4]);
+static void M_CheatWarpCarryOver(player_t* player, int8_t dat[4]);
 static void M_CheatMyPos(player_t* player, int8_t dat[4]);
 static void M_CheatAllMap(player_t* player, int8_t dat[4]);
 
@@ -61,6 +62,7 @@ cheatinfo_t cheat[] = {
 	{   "idkfa",    M_CheatKfa,         0   },
 	{   "idclip",   M_CheatClip,        0   },
 	{   "idclev",   M_CheatWarp,        -2  },
+	{   "exclev",   M_CheatWarpCarryOver, -2  },
 	{   "idpos",    M_CheatMyPos,       0   },
 	{   "exm",		M_CheatAllMap,      0   },
 	{   "exr",		M_CheatBerserk,     0   },
@@ -142,6 +144,35 @@ static void M_CheatBerserk(player_t* player, int8_t dat[4]) {
 
 CVAR_EXTERNAL(sv_skill);
 static void M_CheatWarp(player_t* player, int8_t dat[4]) {
+	char	lumpname[9];
+	int		lumpnum;
+	int map;
+	map = datoi(dat);
+	gameskill = (int)sv_skill.value;
+	gamemap = nextmap = map;
+
+	if (map < 1)
+	{
+		return;
+	}
+	if (map < 10)
+	{
+		DEH_snprintf(lumpname, 9, "MAP0%i", map);
+	}
+	else {
+		DEH_snprintf(lumpname, 9, "MAP%i", map);
+	}
+	lumpnum = map ? W_GetNumForName(lumpname) : W_CheckNumForName(lumpname);
+
+	if (lumpnum)
+	{
+		// So be it.
+		G_InitNew(gameskill, map);
+		dmemset(passwordData, 0xff, 16);
+	}
+}
+
+static void M_CheatWarpCarryOver(player_t* player, int8_t dat[4]) {
 	char	lumpname[9];
 	int		lumpnum;
 	int map;
