69687d6fcf289f9cdafd42ac2966958acb4b1fe4
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2023-01-12 10:55:42 -0300

    more fix

--- engine/p_pspr.c
+++ engine/p_pspr.c
@@ -929,7 +929,9 @@ void T_LaserThinker(laserthinker_t* laserthinker) {
 //
 // A_FireLaser
 //
-
+fixed_t laserhit_x;
+fixed_t laserhit_y;
+fixed_t laserhit_z;
 
 
 void A_FireLaser(player_t* player, pspdef_t* psp) {
@@ -945,9 +947,7 @@ void A_FireLaser(player_t* player, pspdef_t* psp) {
 	unsigned char            type;
 	laser_t* laser[3];
 	laserthinker_t* laserthinker[3];
-	fixed_t laserhit_x;
-	fixed_t laserhit_y;
-	fixed_t laserhit_z;
+
 	//fixed_t         laserfrac;
 
 	mobj = player->mo;
@@ -1010,8 +1010,8 @@ void A_FireLaser(player_t* player, pspdef_t* psp) {
 		laser[i] = Z_Malloc(sizeof(*laser[i]), PU_LEVSPEC, 0);
 
 		// setup laser head point
-		laser[i]->x1 = mobj->x + FixedMul(LASERDISTANCE, cos(mobj->angle));
-		laser[i]->y1 = mobj->y + FixedMul(LASERDISTANCE, sin(mobj->angle));
+		laser[i]->x1 = mobj->x + FixedMul(LASERDISTANCE, dcos(mobj->angle));
+		laser[i]->y1 = mobj->y + FixedMul(LASERDISTANCE, dsin(mobj->angle));
 		laser[i]->z1 = (mobj->z + LASERAIMHEIGHT);
 
 		// setup laser tail point
@@ -1020,8 +1020,8 @@ void A_FireLaser(player_t* player, pspdef_t* psp) {
 		laser[i]->z2 = laserhit_z;
 
 		// setup movement slope
-		laser[i]->slopex = (cos(angleoffs) << 5);
-		laser[i]->slopey = (sin(angleoffs) << 5);
+		laser[i]->slopex = (dcos(angleoffs) << 5);
+		laser[i]->slopey = (dsin(angleoffs) << 5);
 		laser[i]->slopez = slope ? (slope << 5) : 0;
 
 		// setup distance info
