974bffa293abc9e66bfbd5952be4a93671c6498f
Wolf3s <75867486+wolfysdl@users.noreply.github.com>
2023-01-12 16:37:59 -0300

    Update gl_texture.c

--- engine/gl_texture.c
+++ engine/gl_texture.c
@@ -2,7 +2,7 @@
 //-----------------------------------------------------------------------------
 //
 // Copyright(C) 2007-2012 Samuel Villarreal
-// Copyright(C) 2022 André Gulherme
+// Copyright(C) 2022-2023 André Gulherme
 // 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
