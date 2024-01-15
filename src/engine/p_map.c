543a9b2b3be39fc4c057fa08a3aa5fa14b83e978
bubbleguuum <youp1one1@gmail.com>
2024-01-15 21:57:33 +0100

    fix func proto mismatches resulting in warnings with -flto (#231)
    Co-authored-by: bubbleguuum <bubbleguuum@free.fr>

--- src/engine/p_map.c
+++ src/engine/p_map.c
@@ -707,7 +707,7 @@ static boolean PIT_CheckMobjZ(mobj_t* thing) {
 // P_CheckOnMobj
 //
 
-void P_ZMovement(mobj_t* mo, int checkmissile);
+extern void P_ZMovement(mobj_t* mo);
 
 mobj_t* P_CheckOnMobj(mobj_t* thing) {
     int    bx, by;
@@ -722,7 +722,7 @@ mobj_t* P_CheckOnMobj(mobj_t* thing) {
     tmthing = thing;
     tmflags = thing->flags;
     oldmo = *thing; // save the old mobj before the fake zmovement
-    P_ZMovement(tmthing, false);
+    P_ZMovement(tmthing);
 
     tmx = x;
     tmy = y;
