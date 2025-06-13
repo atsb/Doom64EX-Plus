d7736f7f7094fce88b8d6b0cf844eebfdfae49de
StevenSYS <139715581+StevenSYS@users.noreply.github.com>
2025-06-13 09:31:57 +0000

    Fixed a graphical glitch when pressing "Apply Settings" in the "Video" menu with accessibility mode enabled

--- src/engine/st_stuff.c
+++ src/engine/st_stuff.c
@@ -482,10 +482,9 @@ void ST_FlashingScreen(byte r, byte g, byte b, byte a) {
 		dglDisable(GL_TEXTURE_2D);
 		dglColor4ubv((byte*)&c);
 		dglRecti(SCREENWIDTH, SCREENHEIGHT, 0, 0);
-		dglEnable(GL_TEXTURE_2D);
-
 		GL_SetState(GLSTATE_BLEND, 0);
 	}
+	dglEnable(GL_TEXTURE_2D);
 }
 
 //
