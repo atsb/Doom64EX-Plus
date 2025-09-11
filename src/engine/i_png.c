6cb80833681b704cc180cde3f3303fce95971196
bubbleguuum <bubbleguuum@free.fr>
2025-09-11 15:55:32 +0200

    add support for recent libpng

--- src/engine/i_png.c
+++ src/engine/i_png.c
@@ -30,7 +30,7 @@
 #include "doomtype.h"
 #include "i_system.h"
 #include "i_swap.h"
-#include "m_fixed.h"
+#include "m_misc.h"
 #include "z_zone.h"
 #include "w_wad.h"
 #include "gl_texture.h"
@@ -342,8 +342,22 @@ byte* I_PNGReadData(int lump, bool palette, bool nopack, bool alpha,
             if (pal && palindex) {
                 int i;
                 if (bit_depth == 4) {
+                    png_colorp dst_pal = pal;
+                    char palname[9];
+                    int pallump;
+
+                    SDL_snprintf(palname, sizeof(palname), "P%s", lumpinfo[lump].name);
+                    pallump = W_CheckNumForName(palname);
+
+                    if(pallump != -1) {
+                        // load palette separately as lump stored in doom64ex-plus.wad if it exists 
+                        // for compatibility with libpng > 1.5.15 not supporting PNG with num_pal > 16 for bit_depth == 4
+                        pal = W_CacheLumpName(palname, PU_STATIC);
+                        num_pal = lumpinfo[pallump].size / sizeof(png_color);
+                    }
+
                     for (i = 0; i < 16 && (16 * palindex + i) < num_pal; i++) {
-                        dmemcpy(&pal[i], &pal[(16 * palindex) + i], sizeof(png_color));
+                        dmemcpy(&dst_pal[i], &pal[(16 * palindex) + i], sizeof(png_color));
                     }
                 }
                 else if (bit_depth >= 8) {
